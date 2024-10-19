2a64188513a4aaacf0d15b190fa8ef05db3c89a9
tripti-angry <tripti23557@iiitd.ac.in>
2024-10-19 12:38:02 +0530

    Update MenuScreen.java

--- AngryBirds/core/src/main/java/io/github/some_example_name/MenuScreen.java
+++ AngryBirds/core/src/main/java/io/github/some_example_name/MenuScreen.java
@@ -1,4 +1,106 @@
 package io.github.some_example_name;
 
-public class MenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class MainMenuScreen implements Screen {
+
+    private final AngryBirds game;
+    private Stage stage;
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private Texture background;
+
+    public MainMenuScreen(final AngryBirds game) {
+        this.game = game;
+        this.batch = game.batch;
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        // Load assets
+        font = new BitmapFont(); // Default LibGDX font
+        background = new Texture("menu_background.png"); // Custom background image
+
+        // Create buttons
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json")); // Skin for buttons
+
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        // Button listeners
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game)); // Switch to game screen
+                dispose();
+            }
+        });
+
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit(); // Close the game
+            }
+        });
+
+        // Table for button layout
+        Table table = new Table();
+        table.center();
+        table.setFillParent(true);
+
+        // Add buttons to table
+        table.add(playButton).padBottom(20).row();
+        table.add(exitButton).padBottom(20).row();
+
+        stage.addActor(table);
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Draw the background and buttons
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        background.dispose();
+        font.dispose();
+    }
 }
