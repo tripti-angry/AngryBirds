280076f74b2bb826563c7793fb6b99f43ad304e5
tripti-angry <tripti23557@iiitd.ac.in>
2024-10-20 12:27:36 +0530

    Update and rename Main.java to MainGame.java

---
+++ AngryBirds/core/src/main/java/io/github/some_example_name/MainGame.java
@@ -0,0 +1,37 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MainGame extends Game {
+
+    public SpriteBatch batch;
+    private HomeScreen homeScreen;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        // Create the HomeScreen instance and set it as the initial screen
+        homeScreen = new HomeScreen(this);
+        this.setScreen(homeScreen);
+    }
+
+    @Override
+    public void render() {
+        super.render();  // Delegates to the active screen's render method
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height); // This will call resize on the current screen
+    }
+
+    @Override
+    public void dispose() {
+        // Dispose of the SpriteBatch and the HomeScreen
+        if (homeScreen != null) {
+            homeScreen.dispose();
+        }
+        batch.dispose();
+    }
+}
