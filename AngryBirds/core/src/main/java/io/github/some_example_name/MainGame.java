e781a1c04b8e4c495c5c5eb7182d9819b02e8343
tripti-angry <tripti23557@iiitd.ac.in>
2024-10-26 22:26:04 +0530

    Update MainGame.java

--- AngryBirds/core/src/main/java/io/github/some_example_name/MainGame.java
+++ AngryBirds/core/src/main/java/io/github/some_example_name/MainGame.java
@@ -3,8 +3,6 @@ package io.github.some_example_name;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-
-
 public class MainGame extends Game {
 
     public SpriteBatch batch;
@@ -13,24 +11,23 @@ public class MainGame extends Game {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        // Create the HomeScreen instance and set it as the initial screen
         homeScreen = new HomeScreen(this);
         this.setScreen(homeScreen);
     }
 
     @Override
     public void render() {
-        super.render();  // Delegates to the active screen's render method
+        super.render();
     }
 
     @Override
     public void resize(int width, int height) {
-        super.resize(width, height); // This will call resize on the current screen
+        super.resize(width, height);
     }
 
     @Override
     public void dispose() {
-        // Dispose of the SpriteBatch and the HomeScreen
+
         if (homeScreen != null) {
             homeScreen.dispose();
         }
