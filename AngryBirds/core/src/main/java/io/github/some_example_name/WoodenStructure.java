7a535de44729d75735ff70296414038012e441b0
Janya05 <147905791+Janya05@users.noreply.github.com>
2024-11-22 15:06:20 +0530

    Update WoodenStructure.java

--- AngryBirds/core/src/main/java/io/github/some_example_name/WoodenStructure.java
+++ AngryBirds/core/src/main/java/io/github/some_example_name/WoodenStructure.java
@@ -16,7 +16,7 @@ public class WoodenStructure extends Structure {
 
     public WoodenStructure(float x, float y, int health, String shapeType, World world) {
         // Initialize the glass structure with a texture based on the shape type
-        super(new TextureRegion(new Texture(getTextureForShape(shapeType))), x, y, health);
+        super(new TextureRegion(new Texture(getTextureForShape(shapeType))), x, y, health,world);
 
         // Set initial size based on shape type or custom scaling
         this.width = 100;  // Default width
