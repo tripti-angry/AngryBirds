22c8d9f51db7bfe042fc62799b58f9e4c147bef2
Janya05 <147905791+Janya05@users.noreply.github.com>
2024-11-29 23:40:36 +0530

    Update SaveData.java

--- AngryBirds/core/src/main/java/io/github/some_example_name/SaveData.java
+++ AngryBirds/core/src/main/java/io/github/some_example_name/SaveData.java
@@ -4,11 +4,11 @@ public class SaveData {
     public int currentLevel;
     public int score;
 
-    // No-argument constructor (required for serialization)
+  
     public SaveData() {
     }
 
-    // Constructor to set values
+    
     public SaveData(int currentLevel, int score) {
         this.currentLevel = currentLevel;
         this.score = score;
